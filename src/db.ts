16542ad53e2cafdbb3933155cbbc27dc7b68c8c6
Lucas Becker <lks.becker@gmail.com>
2023-11-13 09:52:13 -0300

    create ngFor items db

---
+++ src/db.ts
@@ -0,0 +1,42 @@
+export const db = [
+  {
+    img: 'assets/ac-cover.jpg',
+    label: 'NEW',
+    type: 'Digital PS5',
+    price: '229,99',
+  },
+
+  {
+    img: 'assets/bt-1.jpg',
+    label: 'EXCLUSIVE',
+    type: 'DISC PS4',
+    price: '169,99',
+  },
+
+  {
+    img: 'assets/bt-4.jpg',
+    label: 'DIGITAL',
+    type: 'Digital PS5',
+    price: '209,99',
+  },
+
+  {
+    img: 'assets/bt-5.jpg',
+    label: 'EXCLUSIVE',
+    type: 'Digital PS4, PS5',
+    price: '269,99',
+  },
+
+  {
+    img: 'assets/bt-bad-company-2.jpg',
+    label: 'NEW',
+    type: 'Digital PS5',
+    price: '239,99',
+  },
+  {
+    img: 'assets/bt-hardline.jpg',
+    label: 'SUCCESS ',
+    type: 'DISC PS4, PS5',
+    price: '219,99',
+  },
+];
